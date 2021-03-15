5c6e673e015dd9667e1c9011c8650c4444aec4a0
Alberto Marfoglia <alberto.marfoglia@studio.unibo.it>
2021-03-15 12:20:10 +0100

    test and implement map based on flatMap

--- src/ulab03/Task1.scala
+++ src/ulab03/Task1.scala
@@ -1,5 +1,5 @@
 package ulab03
-import u03.Lists.List.{Cons, append}
+import u03.Lists.List.{Cons, Nil, append}
 import u03.Lists._
 
 import scala.annotation.tailrec
@@ -7,12 +7,14 @@ import scala.annotation.tailrec
 object Task1 {
   @tailrec
   def drop[A](l: List[A], n: Int): List[A] = l match {
-    case _ if n<1 || l == List.Nil() => l
+    case _ if n<1 || l == Nil() => l
     case Cons(_, t)  => drop(t, n-1)
   }
 
   def flatMap[A,B](l: List[A])(f: A => List[B]): List[B] = l match {
     case Cons(h, t) => append(f(h), flatMap(t)(f))
-    case _ => List.Nil()
+    case _ => Nil()
   }
+
+  def map[A,B](l: List[A])(mapper: A=>B): List[B] = flatMap(l)(x => Cons(mapper(x), Nil()))
 }
--- test/ulab03/Task1Test.scala
+++ test/ulab03/Task1Test.scala
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import u03.Lists._
 import org.junit.jupiter.api.Test
 import u03.Lists.List.Cons
-import ulab03.Task1.{drop, flatMap}
+import ulab03.Task1.{drop, flatMap, map}
 
 class Task1Test {
   val lst: Cons[Int] = Cons(10, Cons(20, Cons(30, List.Nil())))
@@ -20,5 +20,12 @@ class Task1Test {
     assertEquals(Cons(11,Cons(21,Cons(31, List.Nil()))), flatMap(lst)(v => Cons(v+1, List.Nil())))
     assertEquals(Cons(11,Cons(12,Cons(21,Cons(22,Cons(31,Cons(32, List.Nil())))))),
       flatMap(lst)(v => Cons(v+1, Cons(v+2, List.Nil()))))
+  }
+
+  @Test
+  def testMap(): Unit = {
+    assertEquals(Cons(11,Cons(21,Cons(31, List.Nil()))), map(lst)(_+1))
+    assertEquals(Cons(20,Cons(40,Cons(60, List.Nil()))), map(lst)(_*2))
+    assertEquals(Cons(-10, Cons(-20, Cons(-30, List.Nil()))), map(lst)(_*-1))
   }
 }
