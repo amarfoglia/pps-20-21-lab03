5206373aca4f0df086815adfe840579872e4ebbd
Alberto Marfoglia <alberto.marfoglia@studio.unibo.it>
2021-03-15 12:06:18 +0100

    test and implement flatMap

--- src/ulab03/Task1.scala
+++ src/ulab03/Task1.scala
@@ -1,5 +1,5 @@
 package ulab03
-import u03.Lists.List.Cons
+import u03.Lists.List.{Cons, append}
 import u03.Lists._
 
 import scala.annotation.tailrec
@@ -9,5 +9,10 @@ object Task1 {
   def drop[A](l: List[A], n: Int): List[A] = l match {
     case _ if n<1 || l == List.Nil() => l
     case Cons(_, t)  => drop(t, n-1)
+  }
+
+  def flatMap[A,B](l: List[A])(f: A => List[B]): List[B] = l match {
+    case Cons(h, t) => append(f(h), flatMap(t)(f))
+    case _ => List.Nil()
   }
 }
--- test/ulab03/Task1Test.scala
+++ test/ulab03/Task1Test.scala
@@ -3,14 +3,22 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import u03.Lists._
 import org.junit.jupiter.api.Test
 import u03.Lists.List.Cons
-import ulab03.Task1.drop
+import ulab03.Task1.{drop, flatMap}
 
 class Task1Test {
+  val lst: Cons[Int] = Cons(10, Cons(20, Cons(30, List.Nil())))
+
   @Test
   def testDrop(): Unit = {
-    val  lst = Cons(10, Cons(20, Cons(30, List.Nil())))
     assertEquals(Cons(20,Cons(30, List.Nil())), drop(lst ,1))
     drop(lst ,2)
     drop(lst ,5)
+  }
+
+  @Test
+  def testFlatMap(): Unit = {
+    assertEquals(Cons(11,Cons(21,Cons(31, List.Nil()))), flatMap(lst)(v => Cons(v+1, List.Nil())))
+    assertEquals(Cons(11,Cons(12,Cons(21,Cons(22,Cons(31,Cons(32, List.Nil())))))),
+      flatMap(lst)(v => Cons(v+1, Cons(v+2, List.Nil()))))
   }
 }
